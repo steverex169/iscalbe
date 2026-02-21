37a149ea33a38f2b94b31f4b7272a7f5b3dbd5c3
saher riaz <72702041+saher786-sudo@users.noreply.github.com>
2026-02-21 11:43:43 +0500

    Update SparklesClean.jsx

--- src/Component/ui/SparklesClean.jsx
+++ src/Component/ui/SparklesClean.jsx
@@ -1,17 +1,36 @@
 "use client";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { SparklesCore } from "../../Component/ui/sparkles";
 
 const SparkleClean = () => {
+  const [particleCount, setParticleCount] = useState(1400);
+
+  useEffect(() => {
+    // Function to update particle count based on window width
+    const updateParticleCount = () => {
+      if (window.innerWidth <= 768) {
+        setParticleCount(900);
+      } else {
+        setParticleCount(1400);
+      }
+    };
+
+    updateParticleCount(); // initial check
+    window.addEventListener("resize", updateParticleCount);
+
+    return () => {
+      window.removeEventListener("resize", updateParticleCount);
+    };
+  }, []);
+
   return (
-    <div className="relative w-full h-[150px] overflow-hidden">
-      
+    <div className="relative w-full h-[1200px] overflow-hidden">
       {/* ⭐ actual particles */}
       <SparklesCore
         background="transparent"
-        minSize={1}
+        minSize={2}
         maxSize={1.5}
-        particleCount={400}
+        particleCount={particleCount}
         particleColor="#ffffff"
         className="w-full h-full"
       />
