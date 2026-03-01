7823d21d883e51708d3e6ba37b4679da4fa8702d
saher riaz <72702041+saher786-sudo@users.noreply.github.com>
2026-03-02 00:05:12 +0500

    Update LastPart.jsx

--- src/Component/LastPart/LastPart.jsx
+++ src/Component/LastPart/LastPart.jsx
@@ -1,42 +1,65 @@
+
 import React from "react";
+import { Widget } from "@typeform/embed-react";
+import foo1 from "../Bottom/images/foo1.png";
+import foo2 from "../Bottom/images/foo2.png";
+import foo3 from "../Bottom/images/foo3.png";
+
+
 import "./LastPart.css";
 
 const LastPart = () => {
   return (
-    <footer className="lastPart">
+    <section className="lastPart">
+
+      {/* LEFT SIDE */}
       <div className="lastPart-left">
-        <div className="lastPart-logo-box">
-          <div className="lastPart-bar lastPart-bar1"></div>
-          <div className="lastPart-bar lastPart-bar2"></div>
-          <div className="lastPart-bar lastPart-bar3"></div>
-          <h2 className="lastPart-brand-text">iscalable</h2>
+
+        <div className="logoRow">
+          <div className="lastPart-logo-box">
+            <div className="lastPart-bar lastPart-bar1"></div>
+            <div className="lastPart-bar lastPart-bar2"></div>
+            <div className="lastPart-bar lastPart-bar3"></div>
+            <h2 className="lastPart-brand-text">iscalable</h2>
+          </div>
         </div>
 
-        <p className="lastPart-desc">
-          A UK-based marketing agency, proudly helping over 300 clients worldwide
-          elevate their brands since 2018.
-        </p>
+        <p className="mainText">
+  To move things forward efficiently, answer a few quick
+  questions so we can see if you're the right fit for our agency.
+</p>
+
+<button className="ctaBtn">
+  Book Your FREE Audit →
+</button>
+<div className="promoCards">
+
+  <div className="promoCard">
+    <img className="img1" src={foo1} alt="card1" />
+  </div>
+
+  <div className="promoCard">
+    <img className="img2" src={foo2} alt="card2" />
+  </div>
+
+  <div className="promoCard">
+    <img className="img3" src={foo3} alt="card3" />
+  </div>
+
+</div>
+
       </div>
 
+      {/* RIGHT SIDE - TYPEFORM */}
       <div className="lastPart-right">
-        <h4 className="lastPart-heading">QUICK LINKS</h4>
-        <div className="lastPart-links-columns">
-          <ul className="lastPart-list">
-            <li>Home</li>
-            <li>Case Studies</li>
-            <li>Our Procedure</li>
-            <li>About Us</li>
-            <li>Contact Us</li>
-          </ul>
-          <ul className="lastPart-list">
-            <li>FAQs</li>
-            <li>Blog posts</li>
-            <li>Portfolio</li>
-            <li>Reviews</li>
-          </ul>
-        </div>
+        <Widget
+          id="YOUR_TYPEFORM_ID"
+          style={{ width: "100%", height: "500px", borderRadius: "20px" }}
+          className="typeformBox"
+        />
       </div>
-    </footer>
+
+    </section>
   );
 };
 
